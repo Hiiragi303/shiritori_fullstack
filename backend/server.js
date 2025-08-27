d0f947a73f5bd54d734f58460de7d1bb6ebfb0ef
hiiragi <123towa.ite@gmail.com>
2025-08-27 13:49:14 +0900

    ✨ express簡易導入

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("server start");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api/health", (req, res) => {
+    res.send("ok");
+});
+
+app.listen(port, () => {
+    console.log(`server start at http:localhost:${port}`);
+});
