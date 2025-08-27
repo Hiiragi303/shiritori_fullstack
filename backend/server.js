c7fb9f355205ad9faafb20dae2a9eff8b642cdc6
hiiragi <123towa.ite@gmail.com>
2025-08-27 14:04:26 +0900

    ✨ フロントからバックエンドを叩く

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+}));
 
 app.get("/api/health", (req, res) => {
     res.send("ok");
